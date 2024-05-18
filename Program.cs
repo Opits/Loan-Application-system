ef648ece92586cffe38f828b4808eb57901b878f
Oratile Pitsoane <124570434+Opits@users.noreply.github.com>
2024-05-19 01:31:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PRG_271
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateLoans loans = new CreateLoans();
+            loans.loanMenu();
+            Console.ReadKey();
+        }
+    }
+}
